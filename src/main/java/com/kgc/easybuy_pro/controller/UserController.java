397cb793b5ba325b6f799d91245eba30cbc8b62a
30959 <1805845059@qq.com>
2025-02-27 15:09:36 +0800

    周兴星

--- src/main/java/com/kgc/easybuy_pro/controller/UserController.java
+++ src/main/java/com/kgc/easybuy_pro/controller/UserController.java
@@ -1,5 +1,9 @@
 package com.kgc.easybuy_pro.controller;
 
+import com.kgc.easybuy_pro.pojo.User;
+import com.kgc.easybuy_pro.service.UserService;
+import com.kgc.easybuy_pro.util.ResponseMsg;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -7,4 +11,20 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("user")
 public class UserController {
 
+    @Autowired
+    private UserService userService;
+
+    @RequestMapping("/login")
+    public ResponseMsg login(String username,String password){
+        ResponseMsg msg = new ResponseMsg();
+        User loginUser = userService.Login(username, password);
+        if(loginUser==null){
+            msg.setCode("301");
+            msg.setData(loginUser);
+        }else{
+            msg.setCode("200");//登录成功
+            msg.setData(loginUser);
+        }
+        return  msg;
+    }
 }
