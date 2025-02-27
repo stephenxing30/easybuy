ca3963314ab7c23d65527e953304055ff1048aef
XJJQAQ <1142707116@qq.com>
2025-02-27 09:29:48 +0800

    测试

---
+++ src/test/java/com/kgc/easybuy_project/EasybuyProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kgc.easybuy_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EasybuyProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
