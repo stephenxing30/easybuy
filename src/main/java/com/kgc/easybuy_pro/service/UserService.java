e53e7658556d48df712fd2c5bcae6ad39554b0fa
30959 <1805845059@qq.com>
2025-02-27 14:15:13 +0800

    周兴星

--- src/main/java/com/kgc/easybuy_pro/service/UserService.java
+++ src/main/java/com/kgc/easybuy_pro/service/UserService.java
@@ -1,4 +1,8 @@
 package com.kgc.easybuy_pro.service;
 
+import com.kgc.easybuy_pro.pojo.User;
+
 public interface UserService {
+    User Login(String username,String password);
+    boolean Register(User user);
 }
--- src/main/java/com/kgc/easybuy_pro/service/impl/UserServiceImpl.java
+++ src/main/java/com/kgc/easybuy_pro/service/impl/UserServiceImpl.java
@@ -1,8 +1,19 @@
 package com.kgc.easybuy_pro.service.impl;
 
+import com.kgc.easybuy_pro.pojo.User;
+import com.kgc.easybuy_pro.service.UserService;
 import org.springframework.stereotype.Service;
 
 @Service
-public class UserServiceImpl {
+public class UserServiceImpl implements UserService {
 
+    @Override
+    public User Login(String username, String password) {
+        return null;
+    }
+
+    @Override
+    public boolean Register(User user) {
+        return false;
+    }
 }
